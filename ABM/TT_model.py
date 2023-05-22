e29bd4ea345e7fe2a6d571b6feb709da6572da92
Luja von Köckritz <l.m.s.vonkockritz@uu.nl>
2023-05-22 17:15:57 +0200

    Added docstring documentation to ABM source code,

--- ABM/TT_model.py
+++ ABM/TT_model.py
@@ -29,6 +29,11 @@ modes_attitudes = ['Attitude_ICECar',
  'Attitude_Bike',
  'Attitude_CarSh']
 
+# Parameters
+age_diff_similarity = 10 # Number of years in which the age similarity influences whether two agents have a link
+
+char_weights = {'c_age': 0.1, 'c_sex': 0.1, 'c_income': 0.1, 'c_education': 0.05, 'c_occupation': 0.05}
+
 
 # %%
 ###############
@@ -37,7 +42,38 @@ modes_attitudes = ['Attitude_ICECar',
               
 
 class UserAgent(Agent):
+    """
+    Represents a user agent in a simulation of the TransportTransform agent-based model.
+
+    Attributes:
+    - unique_id (int): Unique identifier for the agent.
+    - model (object): The simulation model object, i.e. amnd instance of the TransportModel class.
+    - group (str): The group the agent belongs to.
+    - preference_1 (str): The agent's first preference for mode choice.
+    - preference_2 (str): The agent's second preference for mode choice.
+    - license (bool): Indicates whether the agent has a license.
+    - car_owner (bool): Indicates whether the agent owns a car.
+    - attributes (dict): Additional attributes of the agent.
+
+    Methods:
+    - step(): Performs the agent's action for a single time step.
+    """
+    
     def __init__(self, unique_id, model, group, preference_1, preference_2, license, car_owner, attributes):
+        """
+        Initialize a UserAgent instance. This represents a single user in the network of UserAgents 
+        on this instance of the TransportModel class
+
+        Parameters:
+        - unique_id (int): Unique identifier for the agent.
+        - model (object): The simulation model object.
+        - group (str): The group the agent belongs to.
+        - preference_1 (str): The agent's first preference for mode choice.
+        - preference_2 (str): The agent's second preference for mode choice.
+        - license (bool): Indicates whether the agent has a license.
+        - car_owner (bool): Indicates whether the agent owns a car.
+        - attributes (dict): Additional attributes of the agent.
+        """
 
         super().__init__(unique_id, model)
         self.ID = unique_id
@@ -79,7 +115,26 @@ class UserAgent(Agent):
         self.time_changed_mode = 0
 
     def step(self):                
-        def preference_mode_choice(mymodes):                   
+        """
+        Perform a step in the agent's decision-making process.
+
+        This method executes the decision-making process for the agent, considering preferences, social pressure, habits, and occupancy. It updates the main mode of the agent based on the decision-making process.
+
+        """
+        
+        def preference_mode_choice(mymodes):       
+            """
+            Choose a mode based on preferences.
+
+            This function selects a mode from the available modes list based on the agent's preferences and their corresponding probabilities.
+
+            Parameters:
+            - mymodes (list): List of available modes.
+
+            Returns:
+            - chosen_mode (str): The selected mode based on preferences.
+            """       
+
             probs = [0.05] * len(mymodes)
             if self.preference_1 in mymodes:
                 idx = mymodes.index(self.preference_1)
@@ -92,6 +147,12 @@ class UserAgent(Agent):
             return random.choices(mymodes, weights=probs, k=1)[0]
         
         def social_pressure(self):
+            """
+            Apply social pressure to the agent's mode selection.
+
+            This function examines the agent's neighbors and calculates the percentage of neighbors with each mode. If enough neighbors share the same mode, the agent may adopt that mode as their current norm.
+
+            """
             
             num_neighbors = len(self.model.G.edges(self.ID))
             
@@ -136,10 +197,16 @@ class UserAgent(Agent):
             return average_probability
         
         def decision_tree():
+            """
+            Execute the decision-making process for the agent.
+
+            This function implements the decision tree for the agent, considering various factors such as preferences, occupancy, and social norms. It updates the agent's main mode accordingly.
+
+            """
             consider_modes = self.available_modes.copy()
             
             # Occupancy module
-            if self.model.crowding_on:
+            if self.model.occupancy_on:
                 last_experiences = [experience for experience in self.experiences if experience[0] >= self.time_changed_mode]
                 sum_last_experiences = sum(experience[2] for experience in last_experiences[1:])
                 if self.model.occupancy_function == 'threshold':                              
@@ -168,7 +235,20 @@ class UserAgent(Agent):
                     self.main_mode = self.adopted_norm  
 
         # evaluate experience from last time step
-        def crowding_last_step(num_same_mode, max_agents, occupancy_parameter):   
+        def occupancy_last_step(num_same_mode, max_agents, occupancy_parameter):   
+            """
+            Evaluate the occupancy level based on the number of agents with the same mode.
+
+            This function calculates the occupancy level based on the number of agents with the same mode and a given occupancy parameter.
+
+            Parameters:
+            - num_same_mode (int): Number of agents with the same mode.
+            - max_agents (int): Total number of agents.
+            - occupancy_parameter (float): Occupancy parameter used to determine occupancy.
+
+            Returns:
+            - occupancy_level (int): Occupancy level (-1 if crowded, 1 if not crowded).
+            """
             
             threshold = max_agents * occupancy_parameter
             if num_same_mode > threshold:
@@ -177,21 +257,78 @@ class UserAgent(Agent):
                 return 1 # It was not crowded
 
         def linear_probability(num_same_mode, max_agents):
+            """
+            Calculate the probability based on a linear relationship.
+
+            This function calculates the probability based on a linear relationship between the number of agents 
+            with the same mode and the total number of agents.
+
+            Parameters:
+            - num_same_mode (int): Number of agents with the same mode.
+            - max_agents (int): Total number of agents.
+
+            Returns:
+            - probability (float): Probability based on a linear relationship.
+            """
+
             # Calculate the probability based on a linear relationship
             probability = num_same_mode / max_agents
             return probability  
         
         def exponential_probability(num_same_mode, max_agents, exponent):
+            """
+            Calculate the probability based on an exponential relationship.
+
+            This function calculates the probability based on an exponential relationship between the number of agents with the same mode, the total number of agents, and a given exponent.
+
+            Parameters:
+            - num_same_mode (int): Number of agents with the same mode.
+            - max_agents (int): Total number of agents.
+            - exponent (float): Exponent for the exponential relationship.
+
+            Returns:
+            - probability (float): Probability based on an exponential relationship.
+            """
+
             # Calculate the probability based on an exponential relationship
             probability = (num_same_mode / max_agents) ** exponent
             return probability
         
         def log_probability(num_same_mode, max_agents, base):
+            """
+            Calculate the probability based on a logarithmic relationship.
+
+            This function calculates the probability based on a logarithmic relationship between the number of agents with the same mode, the total number of agents, and a given base.
+
+            Parameters:
+            - num_same_mode (int): Number of agents with the same mode.
+            - max_agents (int): Total number of agents.
+            - base (float): Base for the logarithmic relationship.
+
+            Returns:
+            - probability (float): Probability based on a logarithmic relationship.
+            """
+            
             # Calculate the probability based on a logarithmic relationship
             probability = np.log(num_same_mode + 1) / np.log(max_agents + 1) / np.log(base)
             return probability
                 
         def sigmoid_probability(x, max_x, base, turning_point_share):
+            """
+            Calculate the probability based on a sigmoid relationship.
+
+            This function calculates the probability based on a sigmoid relationship between a given value, the maximum value, a base, and a turning point share.
+
+            Parameters:
+            - x (float): Value for the sigmoid calculation.
+            - max_x (float): Maximum value for the sigmoid calculation.
+            - base (float): Base for the sigmoid calculation.
+            - turning_point_share (float): Share of the maximum value determining the turning point of the sigmoid.
+
+            Returns:
+            - probability (float): Probability based on a sigmoid relationship.
+            """
+            
             turning_point = max_x * turning_point_share
             exponent = -base * (x - turning_point)
             probability = 1 / (1 + np.exp(exponent))
@@ -212,20 +349,24 @@ class UserAgent(Agent):
                     self.habit_next -= 1
                 else:
                     # Only consider changing the mode if habit time is up
-                    decision_tree()                                  
+                    decision_tree() # Perform the decision tree function
+                                             
                     
                     # reset habit times
                     self.habit_next = self.habit # reset habit time
                     self.time_changed_mode = self.model.currentStep # Record time the habit changed 
             else:
                 #To evaluate effect of leaving habit out
-                decision_tree()            
+                decision_tree()  # Perform the decision tree function
+                      
       
-            if self.model.crowding_on: 
+            if self.model.occupancy_on: 
+                # Evaluate the occupancy level for the agent's mode from the previous step
+   
                 num_same_mode = self.model.mode_counts.loc[self.model.currentStep - 1, self.main_mode]
                 max_agents = self.model.num_agents
                 if self.model.occupancy_function == 'threshold':
-                    exp = crowding_last_step(num_same_mode, max_agents, self.model.occupancy_parameter)                
+                    exp = occupancy_last_step(num_same_mode, max_agents, self.model.occupancy_parameter)                
                 elif self.model.occupancy_function == 'linear':
                     exp = linear_probability(num_same_mode, max_agents)
                 elif self.model.occupancy_function == 'exponential':
@@ -247,13 +388,36 @@ class UserAgent(Agent):
 class TransportModel(Model):
     def __init__(self, include_all, number_agents_included, step_number, preference_1_prob, preference_2_prob, 
                  habit_on, cluster_factor, 
-                 crowding_on, occupancy_function, occupancy_parameter,  
+                 occupancy_on, occupancy_function, occupancy_parameter,  
                  norms_on, norm_threshold, norm_prob,
                  network_type = 'random'):
         
-        self.num_agents = number_agents_included #  unless include_all is True
+        """
+        Initializes a TransportModel object.
+
+        Args:
+            include_all (bool): Indicates whether to include all agents.
+            number_agents_included (int): Number of agents to include if `include_all` is False.
+            step_number (int): Total number of steps in the model.
+            preference_1_prob (float): Probability of preference 1 for agents.
+            preference_2_prob (float): Probability of preference 2 for agents.
+            habit_on (bool): Indicates whether habit is enabled for agents.
+            cluster_factor (float): Cluster factor used for creating agent networks.
+            occupancy_on (bool): Indicates whether occupancy module is enabled.
+            occupancy_function (str): Function used to calculate occupancy probability.
+            occupancy_parameter (float or tuple): Parameter(s) for the occupancy function.
+            norms_on (bool): Indicates whether the social norms module is enabled.
+            norm_threshold (float): Norm threshold value.
+            norm_prob (float): Probability of norm adherence.
+            network_type (str, optional): Type of network to create. Defaults to 'random'.
+        """
+        
+        
         if include_all:
             print(self.num_agents)
+            self.num_agents = len(TT_Model_Data) 
+        else: 
+            self.num_agents = number_agents_included #  unless include_all is True
         self.step_number = step_number
         self.network_type = network_type  
         self.currentStep = 0    
@@ -268,9 +432,8 @@ class TransportModel(Model):
         self.preference_2_prob = preference_2_prob
         self.habit_on = habit_on
         self.cluster_factor = cluster_factor
-        self.crowding_on = crowding_on
+        self.occupancy_on = occupancy_on
         self.occupancy_function = occupancy_function
-        print(self.occupancy_function)
         self.occupancy_parameter = occupancy_parameter
         self.norms_on = norms_on
         self.norm_threshold = norm_threshold 
@@ -284,7 +447,6 @@ class TransportModel(Model):
         pref_index = pd.MultiIndex.from_product([modes, ['pref_1', 'pref_2']])
         self.pref_counts = pd.DataFrame(index= pref_index, columns = range(self.step_number) )
 
-    
         exclude_list = ['ID','group','preference_1','preference_2','license','car_owned','ID.1','Original_ID'] 
 
         self.agent_id = 0  # Initialize the agent ID variable
@@ -299,12 +461,35 @@ class TransportModel(Model):
         
 
         def calculate_weight(agent, neighbor):
+            """
+            Calculate the weight between two agents based on the differences in their attitudes.
+
+            Parameters:
+                agent (Agent): The first agent.
+                neighbor (Agent): The neighboring agent.
+
+            Returns:
+                float: The weight representing the dissimilarity between the agents.
+            """
+    
             diffs = [abs(agent.attitudes[mode] - neighbor.attitudes[mode]) for mode in modes_attitudes]
             weight = np.sqrt(sum([diff ** 2 for diff in diffs]))
 
             return weight  
         
         def calculate_similarity(agent, neighbor, weights):
+            """
+            Calculate the similarity score between two agents based on their characteristics and weights.
+
+            Parameters:
+                agent (Agent): The first agent.
+                neighbor (Agent): The neighboring agent.
+                weights (dict): A dictionary of weights for different characteristics.
+
+            Returns:
+                float: The similarity score between the agents.
+            """
+            
             similarity_score = 0
             #print("checking similarity")
             for attribute in agent.characteristics:
@@ -346,14 +531,37 @@ class TransportModel(Model):
  
 
     def step(self):
+        """
+        Advance the model by one step.
+
+        This function performs agent steps, counts the number of agents in each mode, counts the number of agents
+        choosing each preference, and updates the corresponding dataframes.
+        """
+
         def count_modes():
+            """
+            Count the number of agents in each mode and update the mode_counts dataframe.
+
+            This function iterates over the agents and counts the number of agents in each mode.
+            The counts are stored in a dictionary with mode as the key and the count as the value.
+            The mode_counts dataframe is then updated with the counts for the current step.
+            """   
+
             counts = {}
             for mode in modes:
                 counts[mode] = sum(agent.main_mode == mode for agent in self.all_agents)
             #print(counts)
             self.mode_counts.loc[self.currentStep] = counts # - 1] = counts
 
-        def count_pref():
+        def count_pref2():
+            """
+            Count the number of agents choosing each preference and update the pref_counts dataframe.
+
+            This function counts the number of agents choosing preference 1 and preference 2.
+            The counts are stored in separate variables.
+            The pref_counts dataframe is then updated with the counts for the current step.
+            """
+
             pref_counts_1 =[]
             pref_counts_2 =[]
             pref_counts_1 = sum(agent.main_mode == agent.preference_1 for agent in self.all_agents)
@@ -362,6 +570,15 @@ class TransportModel(Model):
             self.pref_counts.loc[self.currentStep,['pref_2']] = pref_counts_2
 
         def count_pref():
+            """
+            Count the number of agents choosing each preference and update the pref_counts dataframe.
+
+            This function iterates over the agents and increments the count for each preference.
+            The counts are stored in a dictionary with preference as the key and the count as the value.
+            The counts dictionary is converted to a pandas dataframe and stacked.
+            The pref_counts dataframe is then updated with the stacked counts for the current step.
+            """
+
             # Initialize empty dictionary to store counts
             counts_dict = {col: {mode: 0 for mode in modes} for col in ['pref_1', 'pref_2']}
             
